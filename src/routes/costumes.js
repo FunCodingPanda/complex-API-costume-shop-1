cdfb2d0226b7db446129bf6a6dc217b49566ac60
Justin Chau <chausicle@gmail.com>
2018-03-11 17:41:22 -0700

    Add CRUD routes

--- src/routes/costumes.js
+++ src/routes/costumes.js
@@ -2,4 +2,10 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers/costumes')
 
+router.post('/', ctrl.create)
+router.get('/', ctrl.getAll)
+router.get('/:id', ctrl.getById)
+router.put('/:id', ctrl.update)
+router.get('/:id', ctrl.deleteById)
+
 module.exports = router
