af0f3a054d03928929b86296041eafa46450b9c0
Justin Chau <chausicle@gmail.com>
2018-03-11 17:06:57 -0700

    Add main costume route

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.disable('x-powered-by')
 if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
 app.use(bodyParser.json())
 
+const costumeRoutes = require('./src/routes/costumes')
+app.use('/costumes', costumeRoutes)
+
+app.use((err, req, res, next) => {
+  const status = err.status
+  res.status(status).json({ error: err })
+})
+
 const listener = () => console.log(`Listening on port ${port}`)
 app.listen(port, listener)
 
