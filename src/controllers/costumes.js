9ae21898d9ab7fadfc41f1282bfbbde0b28c7171
Justin Chau <chausicle@gmail.com>
2018-03-11 17:41:42 -0700

    Add callback functions in controllers

--- src/controllers/costumes.js
+++ src/controllers/costumes.js
@@ -1,3 +1,23 @@
 const model = require('../models/costumes')
 
-module.exports = {}
+create = (req, res, next) => {
+
+}
+
+getAll = (req, res, next) => {
+
+}
+
+getById = (req, res, next) => {
+
+}
+
+update = (req, res, next) => {
+
+}
+
+deleteById = (req, res, next) => {
+
+}
+
+module.exports = { create, getAll, getById, update, deleteById }
