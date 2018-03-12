0e8603dc54b38b568f28e30c4877db787a9b027e
Justin Chau <chausicle@gmail.com>
2018-03-11 17:40:55 -0700

    Add error handling

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ const costumeRoutes = require('./src/routes/costumes')
 app.use('/costumes', costumeRoutes)
 
 app.use((err, req, res, next) => {
-  const status = err.status
+  const status = err.status || 500
   res.status(status).json({ error: err })
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' } })
 })
 
 const listener = () => console.log(`Listening on port ${port}`)
