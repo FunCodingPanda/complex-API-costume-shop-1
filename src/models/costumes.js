56125d80a524a1129c8789a7bd0167d601367ef7
Justin Chau <chausicle@gmail.com>
2018-03-11 17:47:00 -0700

    Add functions in models

--- src/models/costumes.js
+++ src/models/costumes.js
@@ -3,4 +3,24 @@ const path = require('path')
 const costumeArray = require('../../costume-shop/costumes.json')
 const uuid = require('uuid/v4')
 
-module.exports = {}
+create = () => {
+
+}
+
+getAll = () => {
+
+}
+
+getById = () => {
+
+}
+
+update = () => {
+
+}
+
+deleteById = () => {
+
+}
+
+module.exports = { create, getAll, getById, update, deleteById }
