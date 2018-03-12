4361784e0b7cf80770d2a41620a7c435f56501d8
Justin Chau <chausicle@gmail.com>
2018-03-12 10:21:11 -0700

    Add MVC folders and files for tag resource

---
+++ src/controllers/tags.js
@@ -0,0 +1,19 @@
+const model = require('../models/tags')
+
+create = (req, res, next) => {
+
+}
+getAll = (req, res, next) => {
+
+}
+getById = (req, res, next) => {
+
+}
+update = (req, res, next) => {
+
+}
+deleteById = (req, res, next) => {
+
+}
+
+module.exports = { create, getAll, getById, update, deleteById }
---
+++ src/models/tags.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+const costumeShop = '../../costume-shop'
+const uuid = require('uuid/v4')
+
+create = () => {
+
+}
+getAll = () => {
+
+}
+getById = () => {
+
+}
+update = () => {
+
+}
+deleteById = () => {
+
+}
+
+module.exports = { create, getAll, getById, update, deleteById }
