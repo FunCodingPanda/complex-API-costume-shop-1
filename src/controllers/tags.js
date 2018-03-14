b24799148cda575e7a406c097261ad05d3cf33b8
Justin Chau <chausicle@gmail.com>
2018-03-13 23:09:47 -0700

    Add controller responses for GET, PUT, and DELETE of tags

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -1,19 +1,71 @@
 const model = require('../models/tags')
 
 create = (req, res, next) => {
+  const tag = model.create(req.params.id, req.body)
 
-}
-getAll = (req, res, next) => {
+  if (tag.errors) {
+     return next({
+       status: tag.status,
+       message: tag.message,
+       errors: tag.errors
+     })
+  } else if (tag.error) {
+       return next({
+         status: tag.status,
+         message: tag.message,
+         error: tag.error
+       })
+    }
 
+  res.status(201).json({ tag })
 }
+
 getById = (req, res, next) => {
+  const tag = model.getById(req.params.id)
 
+  if (tag.error) {
+    return next({
+      status: tag.status,
+      message: tag.message,
+      error: tag.error
+    })
+  }
+
+  res.status(200).json({ tag })
 }
+
 update = (req, res, next) => {
+  const tag = model.update(req.params.id, req.params.tagId, req.body)
 
+  if (tag.errors) {
+     return next({
+       status: tag.status,
+       message: tag.message,
+       errors: tag.errors
+     })
+  } else if (tag.error) {
+       return next({
+         status: tag.status,
+         message: tag.message,
+         error: tag.error
+       })
+    }
+
+  res.status(200).json({ tag })
 }
+
 deleteById = (req, res, next) => {
+  const tag = model.deleteById(req.params.id, req.params.tagId)
 
+  if (tag.error) {
+    return next({
+      status: tag.status,
+      message: tag.message,
+      error: tag.error
+    })
+  }
+
+  res.status(204).json()
 }
 
-module.exports = { create, getAll, getById, update, deleteById }
+module.exports = { create, getById, update, deleteById }
